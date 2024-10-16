764a876b8bd421564935a2be111c545a5bb49acd
FawzyFathi01 <fawzyfathi6@gmail.com>
2024-10-16 07:17:37 +0300

    First Commit

--- OnlineCoursesApp/OnlineCoursesApp.DAL/obj/Debug/net8.0/OnlineCoursesApp.DAL.AssemblyInfo.cs
+++ OnlineCoursesApp/OnlineCoursesApp.DAL/obj/Debug/net8.0/OnlineCoursesApp.DAL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OnlineCoursesApp.DAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72f6d9a610255e09a507c25783a502766495b674")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1042edecd9feac51ffcc85b26ca0e78fda44fa84")]
 [assembly: System.Reflection.AssemblyProductAttribute("OnlineCoursesApp.DAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OnlineCoursesApp.DAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
