cfc2d6c2eef3cd1e64cba6c3f6a0cc1430b345e3
FawzyFathi01 <fawzyfathi6@gmail.com>
2024-10-23 14:35:10 +0300

    Add View For About , Change Layout

--- OnlineCoursesApp/OnlineCoursesApp/ViewModel/HomePageViewModels/CourseHomePageViewModel.cs
+++ OnlineCoursesApp/OnlineCoursesApp/ViewModel/HomePageViewModels/CourseHomePageViewModel.cs
@@ -1,16 +1,12 @@
 ﻿namespace OnlineCoursesApp.ViewModel.HomePageViewModels
 {
-
     public class CoursesHomeViewModel
     {
-        public int CourseId { get; set; } 
+        public int CourseId { get; set; }
         public string? CourseName { get; set; }
-        public string CourseImage {  get; set; }
+        public string CourseImage { get; set; }
         public int NumStudent { get; set; }
-
-        //public string Type { get; set; } //will be enum letter
-        public string InsrUctorName {  get; set; }
-        public string CourseDescription {  get; set; }
         public string InstructorName { get; set; } // اسم المدرس
+        public string CourseDescription { get; set; }
     }
 }
